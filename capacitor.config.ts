844a7feee3009d0487ccb298e575abcdeb0437c1
armelnkana7 <armelnkana7@gmail.com>
2026-02-24 03:08:03 +0100

    Activation du mode PWA

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.adbwallet.app',
+  appName: 'ADBWALLET',
+  webDir: 'build'
+};
+
+export default config;
